675d3d212e3e83dff4bc597b43e0ab2c62635d90
yuI4140 <greyberzambrano79@gmail.com>
2024-10-24 08:58:10 +0200

    Release V0.0.3
    - adding multithreading mode

--- mvi.c
+++ mvi.c
@@ -1,10 +1,10 @@
-#include <getopt.h>
 #include <libgen.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <threads.h>
 #include <unistd.h>
 
 #define GLOB_IMPLEMENTATION
@@ -13,12 +13,15 @@
 #include "./nob.h/nob.h"
 #define YUIS_UTILS
 #include "./yuis-headers/yuis_utils.h"
+#include <pthread.h>
 
 bool is_force = false;
 bool is_pedantic = false;
 bool is_backup = true;
 #define VERSION "0.0.2"
 #define NOB_FOREACH(array) for (size_t i = 0; i < array.count; ++i)
+#define N_THREAD 1
+typedef void*(*vFunc)(void*);
 
 int nob_file_exists_in_dir(const char *filepath, const char *dir) {
   Nob_File_Paths ddir = {0};
@@ -126,6 +129,49 @@ void remove_leading_dashes(char *str) {
         }
     }
 }
+typedef struct Thread {
+    pthread_t th;
+} Thread;
+
+typedef struct ThreadPool {
+    Thread *pool;
+    size_t in_used;
+    size_t n_threads;
+} ThreadPool;
+
+ThreadPool thread_pool_init(size_t n_threads) {
+    ThreadPool thread_pool = {0};
+    thread_pool.n_threads=n_threads;
+    thread_pool.pool = nob_temp_alloc(sizeof(Thread) * n_threads);
+    memset(thread_pool.pool, 0, sizeof(Thread) * n_threads);
+    return thread_pool;
+}
+
+int thread_pool_run(ThreadPool *pool, vFunc fn, void *args) {
+    if (pthread_create(&pool->pool[pool->in_used].th, NULL, fn, args)) {
+        nob_log(NOB_ERROR, "Error creating thread %zu", pool->in_used);
+        return 1;
+    }
+    ++pool->in_used;
+    return 0;
+}
+
+void thread_pool_join(ThreadPool *pool) {
+    for (size_t i = 0; i < pool->in_used; ++i) {
+        pthread_join(pool->pool[i].th, NULL);
+    }
+}
+
+typedef struct MoveArgs {
+    const char *file;
+    const char *dest_dir;
+} MoveArgs;
+
+void *move_file_thread(void *arg) {
+    MoveArgs *args = (MoveArgs *)arg;
+    move_file(args->file, args->dest_dir);
+    return NULL;
+}
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     nob_log(NOB_ERROR, "no arguments given or missing destination-dir!");
@@ -150,9 +196,11 @@ int main(int argc, char *argv[]) {
       nob_log(NOB_INFO,
               "  -f, --force: Force move, overwriting files if they exist");
       nob_log(NOB_INFO, "  -p, --pedantic: Verbose mode");
+      nob_log(NOB_INFO, "  -j<n_threads>, --jobs <n_threads>: multithreading mode");
       return 0;
     }
   }
+  size_t n_threads=1;
   if (argc > 2) {
     for (size_t i = 1; i < argc - 2; ++i) {
       char *opt = strdup(argv[i]);
@@ -166,9 +214,15 @@ int main(int argc, char *argv[]) {
         is_pedantic = true;
       }else if ((strcmp(opt, "no-backup")) == 0) {
         is_backup = false;
+      } else if ((strcmp(opt, "jobs")) == 0 || (strcmp(opt, "j")) == 0) {
+         n_threads=strtol(argv[i+1],NULL,10); 
+      } else if (GLOB_MATCHED==glob_utf8("j*",opt)) {
+          char ch[2]={0}; ch[0]=opt[1]; ch[1]='\0';
+         n_threads=strtol(ch,NULL,10); 
       }
     }
   }
+  ThreadPool thread_pool=thread_pool_init(n_threads);
   const char *param1 = argv[argc - 2];
   const char *dest_dir = argv[argc - 1];
   struct stat dest_stat;
@@ -180,9 +234,10 @@ int main(int argc, char *argv[]) {
   Nob_File_Paths verified_files = {0};
 
   if (nob_file_exists(param1)) {
-    if (move_file(param1, dest_dir) < 0) {
-      return -1;
-    }
+    MoveArgs *args = nob_temp_alloc(sizeof(MoveArgs));
+    args->file = param1;
+    args->dest_dir = dest_dir;
+    thread_pool_run(&thread_pool, move_file_thread, args);
   } else {
     Nob_File_Paths current_dir = {0};
     if (!nob_read_entire_dir(".", &current_dir)) {
@@ -204,12 +259,12 @@ int main(int argc, char *argv[]) {
       return -1;
     }
     NOB_FOREACH(verified_files) {
-      const char *file = verified_files.items[i];
-      if (move_file(file, dest_dir) < 0) {
-        return -1;
-      }
+     const char *file = verified_files.items[i];
+     MoveArgs *args = nob_temp_alloc(sizeof(MoveArgs));
+     args->file = file;
+     args->dest_dir = dest_dir;
+     thread_pool_run(&thread_pool, move_file_thread, args); 
     }
   }
-
   return EXIT_SUCCESS;
 }
